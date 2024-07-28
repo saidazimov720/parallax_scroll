d0ca418428b3213537e31235a497551fe7f80f19
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-28 16:25:02 +0500

    query Selector all

--- js/app.js
+++ js/app.js
@@ -4,3 +4,8 @@ const swiper = new Swiper('.swiper',{
     speed:1700,
     parallax: true
 })
+document.querySelectorAll('.header_content h1').forEach(e =>{
+    e.innerHTML = e.textContent.replace(/ (-|#|){1}/g, s => s[1]+s[0]).replace(/(\S*)/g, m =>{
+        return m.replace(/\S(-|#)?/g, '<span class="letter">$&</span>')
+    })
+})
