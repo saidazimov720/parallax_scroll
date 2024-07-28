8d630e6e5ae0f571594ade39a29d191cfa61194d
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-28 19:03:28 +0500

    query selector for .letter

--- js/app.js
+++ js/app.js
@@ -8,4 +8,7 @@ document.querySelectorAll('.header_content h1').forEach(e =>{
     e.innerHTML = e.textContent.replace(/ (-|#|){1}/g, s => s[1]+s[0]).replace(/(\S*)/g, m =>{
         return m.replace(/\S(-|#)?/g, '<span class="letter">$&</span>')
     })
+    e.querySelectorAll('.letter').forEach(function(l, i) {
+		l.setAttribute('style', `z-index: -${ i }; transition-duration: ${ i/5 + 1 }s`);
+	})
 })
